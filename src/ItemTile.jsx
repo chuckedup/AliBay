984c1f399e41e56bd3daec8d98bfac861a38913b
jzulkarn <51467644+jzulkarn@users.noreply.github.com>
2019-08-15 15:55:22 -0400

    updated ItemTile component

--- src/ItemTile.jsx
+++ src/ItemTile.jsx
@@ -13,4 +13,43 @@ class UnconnectedItemTile extends Component {
 
 let ItemTile = connect()(UnconnectedItemTile);
 
+class Item extends Component {
+  addCart = () => {
+    console.log(this.props.item.id);
+    this.props.method(
+      this.props.item.id,
+      this.props.item.description,
+      this.props.item.imageLocation,
+      this.props.item.cost
+    );
+  };
+  render() {
+    return (
+      <div className="card center">
+        <div className="left">
+          {" "}
+          <img height="100px" src={this.props.item.imageLocation} />
+        </div>
+        <div className="right">
+          <div>{this.props.item.description}</div>
+          <div>{this.props.item.cost}</div>
+          <div>
+            <Link to={"/seller/" + this.props.item.sellerId}>
+              Link to Seller
+            </Link>
+          </div>
+          <div>
+            <Link to={"/details/" + this.props.item.id}>Details</Link>
+          </div>
+          <div>
+            <button onClick={this.addCart} className="cartButton">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default ItemTile;
